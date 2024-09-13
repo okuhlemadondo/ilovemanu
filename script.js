d146f53e7a1d187dc15dac1b261cab3596162cf7
okuhlemadondo <okuhlemadondo@icloud.com>
2024-09-13 19:55:08 +0200

    Completed first swipe down

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 let loader_tl = gsap.timeline();
 let svg_animate = gsap.timeline();
 
@@ -47,9 +48,9 @@ svg_animate.fromTo(
 gsap.fromTo(
   ".quotes",
   {
-    x: "-100%",
+    x: "-50%",
   },
-  { x: "0%", duration: 20, delay: 4, repeat: Infinity, ease: "none" }
+  { x: "50%", duration: 20, delay: 4, repeat: Infinity, ease: "none" }
 );
 
 gsap.to("#swipe-down > img", {
@@ -58,4 +59,84 @@ gsap.to("#swipe-down > img", {
   duration: 0.7,
   repeat: Infinity,
   yoyo: true,
+});
+
+const button = document.getElementById("swipe-down");
+
+button.addEventListener("click", function () {
+  gsap.to(window, {
+    duration: 4,
+    scrollTo: { y: ".section-two" },
+    ease: "power1.inOut",
+  });
+});
+
+gsap.to("#meet, #gf", {
+  scrollTrigger: {
+    trigger: "#meet, #gf",
+    scrub: 1,
+    start: "-80px top",
+  },
+  "font-size": "0rem",
+});
+
+gsap.to(".her > img", {
+  scrollTrigger: {
+    trigger: ".her > img",
+    scrub: true,
+    start: "-90px top",
+  },
+  opacity: 0,
+});
+
+gsap.to("#heart", {
+  scrollTrigger: {
+    trigger: "#heart",
+    scrub: 1,
+    start: "-80px top",
+    end: "-30px top",
+  },
+  y: 90,
+  height: 0.2,
+});
+
+gsap.to(".section-two", {
+  scrollTrigger: {
+    trigger: ".section-two",
+    scrub: 1,
+    start: "top 80%",
+    end: "top 10%",
+  },
+  background:
+    "linear-gradient(to top, rgb(119, 0, 132) 0.1%, rgb(0,0,0) 99.9%)",
+});
+
+gsap.to(".quotes > div > p", {
+  scrollTrigger: {
+    trigger: ".quotes > div > p",
+    scrub: 1,
+    start: "top 80%",
+    end: "top 10%",
+  },
+  color: "black",
+});
+
+gsap.to(".carousel", {
+  scrollTrigger: {
+    trigger: ".carousel",
+    scrub: 1,
+    start: "top 80%",
+    end: "top 10%",
+  },
+  "border-block": "1px solid rgb(0, 0, 0)",
+});
+
+gsap.to("#swipe-down > img", {
+  scrollTrigger: {
+    trigger: "#swipe-down > img",
+    scrub: 1,
+    start: "top 80%",
+    end: "top 10%",
+  },
+  height: "0rem",
 });
